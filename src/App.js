944778c4c73e12fe377b26892dc91b87caf6a2e6
Jyothi <udupaj@gmail.com>
2020-12-16 12:04:11 +0530

    tese-commit1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header>
         <a href="/">Shopping Cart</a>
       </header>
-      <main>Product List</main>
+      <main>test Product List</main>
       <footer>All right is reserved.</footer>
     </div>
   );
